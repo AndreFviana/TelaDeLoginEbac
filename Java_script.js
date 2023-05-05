607f2c0312486caf6c8e01926905a54099626d5f
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2023-05-04 22:57:54 -0300

    Separação do html e java script

---
+++ Java_script.js
@@ -0,0 +1,12 @@
+<script>
+    function logar(){
+      let login = document.getElementById('login').value;
+      let senha = document.getElementById('senha').value;
+
+      if (login == "admin" && senha == "admin"){
+        alert('Tudo certo');
+      } else {
+        alert('Acesso Negado: senha ou login incorreto');
+      }
+    }
+  </script>
